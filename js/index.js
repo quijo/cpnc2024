16fe5394d94d6365b22410ebe888e6d4f3d5c0a7
John Quiachon <rjohnquia@gmail.com>
2024-02-15 17:11:48 +0800

    initial commit

---
+++ js/index.js
@@ -0,0 +1,18 @@
+const menu = document.getElementById('menu')
+const toggleMenu = document.getElementById('menu-list')
+const arrow = document.getElementById('arrow-left')
+
+
+
+
+
+menu.addEventListener('click',function(){
+    toggleMenu.classList.toggle('menu-active');
+
+})
+
+function hide(){
+    toggleMenu.classList.toggle('menu-active')
+    arrow.classList.toggle('active')
+}
+
